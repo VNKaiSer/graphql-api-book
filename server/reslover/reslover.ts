325e2f6f6c59b1d4dfc1f221a9ede2d1933c86c6
kaiser <tandatvok16@gmail.com>
2023-08-20 18:54:53 +0700

    add query child

--- server/reslover/reslover.ts
+++ server/reslover/reslover.ts
@@ -5,7 +5,16 @@ const reslover = {
         book: (parent: any, args: { id: any; }) => books.find((book: { id: any; }) => book.id == args.id),
         authors: () => authors,
         author: (parent: any, args: { id: number; }) => authors.find((author: { id: number; }) => author.id == args.id)
+    },
+
+    Book: {
+        author: (parent: any, args: any) => {
+            return authors.find((author: { id: number; }) => author.id == parent.authorID)
+        }
+
     }
+
+
 }
 
 export default reslover;
